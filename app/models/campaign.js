381e33bdef58377977f76f04f17d988772b6c032
Will Sheehan <wsheehan@bates.edu>
2016-07-07 15:23:21 -0400

    Campaign New

---
+++ app/models/campaign.js
@@ -0,0 +1,11 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+// import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+	title: attr('string'),
+	body: attr('string'),
+	updatedTitle: Ember.computed('title', function() {
+		return `${this.get('title')}`;
+	})
+});
