20d89a8078e504d80e054087969cbd263066d9b9
Will Sheehan <wsheehan@bates.edu>
2016-07-11 13:10:01 -0400

    hotfix

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    sassOptions: {
-      //includePaths: ['app']
-    }
+    // Add options here
   });
 
   // Use `app.import` to add additional libraries to the generated
