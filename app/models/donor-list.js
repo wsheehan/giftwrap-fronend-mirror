9d90b2d98413516fa682da34cb86224fbd79bed5
Will Sheehan <wsheehan@bates.edu>
2016-07-12 14:15:15 -0400

    binding and campaign-create action

--- app/models/donor-list.js
+++ app/models/donor-list.js
@@ -3,9 +3,5 @@ import attr from 'ember-data/attr';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
-	title: attr('string'),
-	body: attr('string'),
-	school_id: attr('number'),
-	user_id: attr('number'),
-	donor_list_id: attr('number')
+	title: attr('string')
 });
