8cc91b1e08ef5e627da81b66eb49dd259227c7c2
Will Sheehan <wsheehan@bates.edu>
2016-08-11 11:46:23 -0400

    Move logic from controller to route, and cleanup

--- app/pods/login/route.js
+++ app/pods/login/route.js
@@ -2,4 +2,19 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+	session: Ember.inject.service('session'),
+
+	setupController(controller) {
+		controller.set('credentials', {});
+	},
+
+	actions: {
+		authenticate(credentials) {
+		  this.get('session').authenticate('authenticator:jwt', credentials).then(() => {
+		  	this.transitionTo('/');
+		  }, () => {
+		  	$("#login-error").html('Invalid Email/Password Combination');
+		  });
+		}
+	}
 });
