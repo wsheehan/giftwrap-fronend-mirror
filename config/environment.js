4a0667f12caad7e3df243be6859f4b3f93ce0ea7
Will Sheehan <wsheehan@bates.edu>
2016-08-11 13:39:38 -0400

    Remove superfluous configs

--- config/environment.js
+++ config/environment.js
@@ -51,14 +51,6 @@ module.exports = function(environment) {
 
   ENV['ember-simple-auth-token'] = {
     serverTokenEndpoint: ENV.APP.API_URL + '/users/authentication',
-    tokenPropertyName: 'token',
-    authorizationPrefix: 'Bearer ',
-    authorizationHeaderName: 'Authorization',
-    headers: {},
-    //refreshAccessTokens: true,
-    //serverTokenRefreshEndpoint: '/users/authentication/refresh',
-    tokenExpireName: 'exp'
-    //refreshLeeway: 0
   };
 
   return ENV;
