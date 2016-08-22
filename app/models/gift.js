49f9095eceacf3d8a7ffdf0fb81d341497847d7d
Will Sheehan <wsheehan@bates.edu>
2016-08-22 13:51:31 -0400

    Gift Creation & Payment Container Component

--- app/models/gift.js
+++ app/models/gift.js
@@ -1,9 +1,11 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
 	total: attr('string'),
 	designation: attr('string'),
-	giftType: attr('string')
+	giftType: attr('string'),
+	donor: belongsTo('donor'),
+	paymentMethodNonce: attr('string')
 });
--- app/pods/components/payment-container/component.js
+++ app/pods/components/payment-container/component.js
@@ -8,35 +8,72 @@ export default Ember.Component.extend({
 			authorization: token
 		}, function(clientErr, clientInstance) {
 			if (clientErr) {
-				alert("Error Creating Client: " + clientErr);
+				console.log(clientErr);
 				return;
 			}
+
+			// Instantiate Paypal
 			Braintree.paypal.create({
 				client: clientInstance
 			}, function(paypalErr, paypalInstance) {
 				if (paypalErr) {
-					alert("Error With Paypal: " + paypalErr);
+					console.log("Paypal Error: " + paypalErr);
 					return;
 				}
 				document.getElementById('paypal-button').addEventListener('click', function () {
-
 					paypalInstance.tokenize({
 						flow: 'vault'
 					}, function (tokenizeErr, payload) {
 
 					if (tokenizeErr) {
 						if (tokenizeErr.type !== 'CUSTOMER') {
-							alert('Error tokenizing: ' + tokenizeErr);
+							console.log(tokenizeErr);
 						}
 						return;
 					}
-
-					console.log('Got a nonce! You should submit this to your server.');
-					console.log(payload.details.email)
+					console.log(payload);
+					document.getElementById('paymentMethodNonce').value = payload.nonce;
 
 					});
 				});
 			});
+
+			// Instantiate Hosted Fields
+			// Braintree.hostedFields.create({
+			// 	client: clientInstance,
+			// 	styles: { 
+			// 		'input.invalid': {
+			// 			"color": "red"
+			// 		},
+			// 		'input.valid': {
+			// 			"color": "green"
+			// 		}
+			// 	},
+			// 	fields: {
+			// 		number: {
+			// 			selector: "#card-number",
+			// 			placeholder: "4111 1111 1111 1111"
+			// 		},
+			// 		expirationDate: {
+			// 			selector: "#exp-date",
+			// 			placeholder: "10 / 2019"
+			// 		}
+			// 	}
+			// }, function (hostedFieldsErr, hostedFieldsInstance) {
+			// 	if (hostedFieldsErr) {
+			// 		alert("Error with Hosted Fields: " + hostedFieldsErr);
+			// 		return;
+			// 	}
+
+			// 	hostedFieldsInstance.tokenize(function (tokenizeErr, payload) {
+			// 		if (tokenizeErr) {
+			// 			alert("(Hosted) Could not tokenize: " + tokenizeErr);
+			// 			return;
+			// 		}
+			// 		console.log(payload)
+			// 	})
+			// });
+
 		});
 	}
 });
