29f45eeca6541edd1bd4565ec8c83f9118f2da7b
Will Sheehan <wsheehan@bates.edu>
2016-08-23 15:40:49 -0400

    Hosted Fields Now Works

--- app/pods/components/payment-container/component.js
+++ app/pods/components/payment-container/component.js
@@ -33,46 +33,94 @@ export default Ember.Component.extend({
 					}
 					console.log(payload);
 					document.getElementById('paymentMethodNonce').value = payload.nonce;
-
+					$("#payment-container").slideUp(400);
+					document.getElementById('paypal-info').style.display = 'block';
+					document.getElementById('paypal-email').innerHTML = '' + payload.details.email + '';
 					});
 				});
+				document.getElementById('paypal-cancel').addEventListener('click', function() {
+					paypalInstance.teardown(function (teardownErr) {
+						if (teardownErr) {
+							alert("Could Not Teardown Method");
+						} else {
+							$("#payment-details").hide(100);
+							$("#payment-container").slideDown(400);
+						}
+					});
+				})
 			});
 
 			// Instantiate Hosted Fields
-			// Braintree.hostedFields.create({
-			// 	client: clientInstance,
-			// 	styles: { 
-			// 		'input.invalid': {
-			// 			"color": "red"
-			// 		},
-			// 		'input.valid': {
-			// 			"color": "green"
-			// 		}
-			// 	},
-			// 	fields: {
-			// 		number: {
-			// 			selector: "#card-number",
-			// 			placeholder: "4111 1111 1111 1111"
-			// 		},
-			// 		expirationDate: {
-			// 			selector: "#exp-date",
-			// 			placeholder: "10 / 2019"
-			// 		}
-			// 	}
-			// }, function (hostedFieldsErr, hostedFieldsInstance) {
-			// 	if (hostedFieldsErr) {
-			// 		alert("Error with Hosted Fields: " + hostedFieldsErr);
-			// 		return;
-			// 	}
+			Braintree.hostedFields.create({
+				client: clientInstance,
+				styles: { 
+					'input': {
+						"font-size": '16px',
+						"color": '#282c37'
+					},
+					'input.invalid': {
+						"color": "#E53A40"
+					},
+					'input.valid': {
+						"color": "green",
+						"border": "green"
+					}
+				},
+				fields: {
+					number: {
+						selector: "#card-number",
+						placeholder: "1111 1111 1111 1111"
+					},
+					expirationDate: {
+						selector: "#exp-date",
+						placeholder: "10 / 2019"
+					},
+					cvv: {
+						selector: "#cvv",
+						placeholder: "123"
+					}
+				}
+			}, function (hostedFieldsErr, hostedFieldsInstance) {
+				if (hostedFieldsErr) {
+					console.log(hostedFieldsErr);
+					return;
+				}
 
-			// 	hostedFieldsInstance.tokenize(function (tokenizeErr, payload) {
-			// 		if (tokenizeErr) {
-			// 			alert("(Hosted) Could not tokenize: " + tokenizeErr);
-			// 			return;
-			// 		}
-			// 		console.log(payload)
-			// 	})
-			// });
+				hostedFieldsInstance.on('validityChange', function (event) {
+			      // Check if all fields are valid, then show submit button
+			      let formValid = Object.keys(event.fields).every(function (key) {
+			        return event.fields[key].isValid;
+			      });
+
+			      if (formValid) {
+			        console.log("Every Field Valid");
+			        hostedFieldsInstance.tokenize(function (tokenizeErr, payload) {
+						if (tokenizeErr) {
+							alert("(Hosted) Could not tokenize: " + tokenizeErr);
+							return;
+						}
+						console.log(payload)
+						document.getElementById('paymentMethodNonce').value = payload.nonce;
+						document.getElementById('card-info').style.display = 'block';
+						//document.getElementById('paypal-email').innerHTML = '' + payload.details.email + '';
+					});
+			      } else {
+			        console.log("Some Fields Still invalid");
+			      }
+			    });
+
+			    document.getElementById('card-cancel').addEventListener('click', function() {
+			    	hostedFieldsInstance.teardown(function (teardownErr) {
+			    		if (teardownErr) {
+							alert("Could Not Teardown Method");
+						} else {
+							$("#payment-details").hide(100);
+							$("#payment-container").slideDown(400);
+						}
+			    	})
+			    })
+
+			});
 
 		});
 	}
